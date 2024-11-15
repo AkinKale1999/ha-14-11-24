cb1963e8a14d96f26b6ca808b81638192e263e2e
AkinKale1999 <akinkale1234@outlook.de>
2024-11-15 11:28:02 +0100

    Terraform-Mit-SSH-An-EC2-Verbinden

--- main.tf
+++ main.tf
@@ -30,23 +30,20 @@ output "instance_public_ips" {
 resource "aws_security_group" "ssh_access" {
   name = "ssh_access"
 
-  ingress = [
-    {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  egress = [
-    {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 
   tags = {
     Name = "ssh_access"
